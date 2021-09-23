044ebaf7ed0d8f2c6a148e388d4e49a92d4ff045
Alexis Lameire <alexis.lameire@gmail.com>
2021-09-23 22:43:39 +0200

    more fix

--- conforhuman/lexer.py
+++ conforhuman/lexer.py
@@ -1,7 +1,8 @@
 from .ast import LocalizableLiteral, FilePosition
 import ply.lex as lex
 
-tokens = (
+class YamlLexer(object):
+    tokens = (
         'COLOM',
         'OPEN_BRACE',
         'CLOSE_BRACE',
@@ -21,137 +22,136 @@ tokens = (
         'NON_QUOTED_STRING'
    )
 
-class YamlLexer(object):
-   def __init__(self):
-       self.lineno = 0
-       self.column = 0
-       self.block = []
-       self.block.append(0)
+    def __init__(self):
+        self.lineno = 0
+        self.column = 0
+        self.block = []
+        self.block.append(0)
 
-   def getPos(self):
-       return FilePosition(self.lineno, self.column)
+    def getPos(self):
+        return FilePosition(self.lineno, self.column)
 
-   def getLiteral(self, t, callack=None):
-       begin = self.getPos()
-       t.column += len(t.value)
-       end = self.getPos()
-       if callback is not None:
-           t.value = callback(t.value)
-       t.value = LocalizableLiteral(begin, end, t.value)
-       return t
+    def getLiteral(self, t, callack=None):
+        begin = self.getPos()
+        t.column += len(t.value)
+        end = self.getPos()
+        if callback is not None:
+            t.value = callback(t.value)
+        t.value = LocalizableLiteral(begin, end, t.value)
+        return t
 
-   def build(self, **kwargs):
-       self.lexer = lex.lex(module=self, **kwargs)
+    def build(self, **kwargs):
+        self.lexer = lex.lex(module=self, **kwargs)
 
-   def t_space(t):
-       'r^[ ]+'
-       indent_size = len(t.value)
-       self.column += current_size
-       if indent_size > self.block[-1]:
-          self.block.append(current_size)
-          t.type = 'BEGIN_BLOCK'
-          return t
-       elif indent_size == self.block:
-          return None
-       elif indent_size != self.block[-2]:
-          raise Exception('bad section indentation')
+    def t_middle_space(self, t):
+        'r^[ ]+'
+        indent_size = len(t.value)
+        self.column += current_size
+        if indent_size > self.block[-1]:
+            self.block.append(current_size)
+            t.type = 'BEGIN_BLOCK'
+            return t
+        elif indent_size == self.block:
+            return None
+        elif indent_size != self.block[-2]:
+            raise Exception('bad section indentation')
        
-       self.block.pop()
+        self.block.pop()
 
-   def t_newline(t):
-       r'\n+'
-       t.lineno += len(t.value)
-       self.column = 0
+    def t_newline(self, t):
+        r'\n+'
+        t.lineno += len(t.value)
+        self.column = 0
 
-   def t_space(t):
-       r'[ ]+'
-       t.column += len(t.value)
+    def t_space(self, t):
+        r'[ ]+'
+        t.column += len(t.value)
 
-   def t_COLOM(self, t):
-       r':'
-       return self.getLiteral(f)
+    def t_COLOM(self, t):
+        r':'
+        return self.getLiteral(f)
 
-   def t_OPEN_BRACE(self, t):
-       r'\{'
-       return self.getLiteral(f)
+    def t_OPEN_BRACE(self, t):
+        r'\{'
+        return self.getLiteral(f)
 
-   def t_CLOSE_BRACE(self, t):
-       r'\}'
-       return self.getLiteral(f)
+    def t_CLOSE_BRACE(self, t):
+        r'\}'
+        return self.getLiteral(f)
 
-   def t_OPEN_BRACKET(self, t):
-       r'\['
-       return self.getLiteral(f)
+    def t_OPEN_BRACKET(self, t):
+        r'\['
+        return self.getLiteral(f)
 
-   def t_CLOSE_BRACKET(self, t):
-       r'\]'
-       return self.getLiteral(f)
+    def t_CLOSE_BRACKET(self, t):
+        r'\]'
+        return self.getLiteral(f)
 
-   def t_COMMA(self, t):
-       r','
-       return self.getLiteral(f)
+    def t_COMMA(self, t):
+        r','
+        return self.getLiteral(f)
    
-   def t_MINUS(self, t):
-       r'-'
-       return self.getLiteral(f)
+    def t_MINUS(self, t):
+        r'-'
+        return self.getLiteral(f)
 
-   def t_INT(self, t):
-       r'[0-9]+'
-       return self.getLiteral(f, int)
+    def t_INT(self, t):
+        r'[0-9]+'
+        return self.getLiteral(f, int)
 
-   def t_FLOAT(self, t):
-       r'[0-9]+\.[0-9]+'
-       return self.getLiteral(f, float)
+    def t_FLOAT(self, t):
+        r'[0-9]+\.[0-9]+'
+        return self.getLiteral(f, float)
 
-   def t_BOOL(self, t):
-       r'(true|false)'
-       begin = self.getPos()
-       if t.value == 'true':
-          t.value = True
-       else:
-          t.value = False
-       self.column+=len(t.value)
-       end = self.getPos()
-       t.value = LocalizableLiteral(begin, end, t.value)
-       return t
+    def t_BOOL(self, t):
+        r'(true|false)'
+        begin = self.getPos()
+        if t.value == 'true':
+            t.value = True
+        else:
+            t.value = False
+        self.column+=len(t.value)
+        end = self.getPos()
+        t.value = LocalizableLiteral(begin, end, t.value)
+        return t
 
-   def t_NULL(self, t):
-       r'null'
-       begin = self.getPos()
-       self.column+=len(t.value)
-       end = self.getPos()
-       t.value = LocalizableLiteral(begin, end, None)
-       return t
+    def t_NULL(self, t):
+        r'null'
+        begin = self.getPos()
+        self.column+=len(t.value)
+        end = self.getPos()
+        t.value = LocalizableLiteral(begin, end, None)
+        return t
 
-   def decode_string(self, t):
-       begin = self.getPos()
-       data = t.value[1:-2].decode('string_escape')
-       self.column+=len(t.value)
-       self.line+=data.count('\n')
-       last_return = t.value.rfind(r'\n')
-       if t.value.rfind(r'\n') >= 0:
-           self.column = len(t.value) - t.value.rfind(r'\n') - 2
-       end = self.getPos()
-       t.value = LocalizableLiteral(begin, end, data)
-       return t
+    def decode_string(self, t):
+        begin = self.getPos()
+        data = t.value[1:-2].decode('string_escape')
+        self.column+=len(t.value)
+        self.line+=data.count('\n')
+        last_return = t.value.rfind(r'\n')
+        if t.value.rfind(r'\n') >= 0:
+            self.column = len(t.value) - t.value.rfind(r'\n') - 2
+        end = self.getPos()
+        t.value = LocalizableLiteral(begin, end, data)
+        return t
 
-   def t_SIMPLE_STRING(self, t):
-       r"'([^']|\')*'"
-       return self.decode_string(t)
+    def t_SIMPLE_STRING(self, t):
+        r"'([^']|\')*'"
+        return self.decode_string(t)
    
-   def t_DOUBLE_STRING(self, t):
-       r'"([^"]|\")"'
-       return self.decode_string(t)
+    def t_DOUBLE_STRING(self, t):
+        r'"([^"]|\")"'
+        return self.decode_string(t)
 
-   def t_NON_QUOTED_STRING(self, t):
-       r"[^ :\"'][^:\"']*"
-       begin = self.getPos()
-       data = t.decode('string_escape')
-       self.column+=len(t.value)
-       self.line+=data.count('\n')
-       last_return = t.value.rfind(r'\n')
-       if t.value.rfind(r'\n') >= 0:
-           self.column = len(t.value) - t.value.rfind(r'\n') - 2
-       end = self.getPos()
-       t.value = LocalizableLiteral(begin, end, data)
-       return t
+    def t_NON_QUOTED_STRING(self, t):
+        r"[^ :\"'][^:\"']*"
+        begin = self.getPos()
+        data = t.decode('string_escape')
+        self.column+=len(t.value)
+        self.line+=data.count('\n')
+        last_return = t.value.rfind(r'\n')
+        if t.value.rfind(r'\n') >= 0:
+            self.column = len(t.value) - t.value.rfind(r'\n') - 2
+        end = self.getPos()
+        t.value = LocalizableLiteral(begin, end, data)
+        return t
--- conforhuman/parser.py
+++ conforhuman/parser.py
@@ -1,47 +1,48 @@
 import ply.yacc as yacc
-from .lexer import tokens, YamlLexer
+from .lexer import YamlLexer
 from .ast import  LocalizableOrderedDict, LocalizableList
+
 
 class YamlParser(object):
     def __init__(self):
         self.lexer = YamlLexer()
         self.lexer.build()
-        self.tokens = tokens
+        self.tokens = self.lexer.tokens
         self.yacc = yacc.yacc(module=self,debug=True)
 
-    def p_document(p):
+    def p_document(self, p):
         '''document : collection
                     | literal'''
         p[0] = p[1]
 
-    def p_literal(p):
+    def p_literal(self, p):
         '''literal : INT
                    | FLOAT
                    | BOOL
                    | string'''
         p[0] = p[1]
 
-    def p_string(p):
+    def p_string(self, p):
         '''string : SIMPLE_STRING
                   | DOUBLE_STRING
                   | NON_QUOTED_STRING'''
         p[0] = p[1]
 
-    def p_collection(p):
+    def p_collection(self, p):
         '''collection : inline_collection
                       | structured_collection '''
         p[0] = p[1]
 
-    def p_inline_collection(p):
+    def p_inline_collection(self, p):
         '''inline_collection : inline_dict
                              | inline_list'''
         p[0] = p[1]
 
-    def p_inline_dict(p):
+    def p_inline_dict(self, p):
         '''inline_dict : OPEN_BRACKET inline_dict_items CLOSE_BRACKET'''
         p[0] = p[2]
 
-    def p_inline_dict_items(p):
+    def p_inline_dict_items(self, p):
         '''inline_dict_item : dict_element
                             | inline_dict_item COMMA dict_element
                             | empty'''
@@ -56,20 +57,20 @@ class YamlParser(object):
            (key, val) = p[3]
            p[0].add(key, val)
 
-    def p_inline_dict_item(p):
+    def p_inline_dict_item(self, p):
         ''' inline_dict_item : string COLOM inline_literal '''
         p[0] = (p[1], p[3])
 
-    def p_inline_literal(p):
+    def p_inline_literal(self, p):
         ''' inline_literal : literal
                            | inline_collection '''
         p[0] = p[1]
 
-    def p_inline_list(p):
+    def p_inline_list(self, p):
         ''' inline_list : OPEN_BRACKET inline_list_items CLOSE_BRACKED'''
         p[0] = p[2]
 
-    def p_inline_list_items(p):
+    def p_inline_list_items(self, p):
         ''' inline_list_items: inline_literal
                              | inline_list_items COMA inline_literal
                              | empty'''
@@ -83,16 +84,16 @@ class YamlParser(object):
            (key, val) = p[3]
            p[0].add(key, val)
 
-    def p_structured_collection(p):
+    def p_structured_collection(self, p):
         '''structured_collection: BEGIN_BLOCK structured_collection_items END_BLOCK'''
         p[0] = p[2]
 
-    def p_structured_collection_items(p):
+    def p_structured_collection_items(self, p):
         '''structured_collection_items: structured_list
                                       | structured_dict '''
         p[0] = p[1]
 
-    def p_structured_list(p):
+    def p_structured_list(self, p):
         '''structured_list : structured_list_item
                            | structured_list structured_list_item
                            | empty'''
@@ -106,11 +107,11 @@ class YamlParser(object):
            (key, val) = p[3]
            p[0].add(key, val)
 
-    def p_structured_list_item(p):
+    def p_structured_list_item(self, p):
         ''' structured_list_item: MINUS document '''
         p[0] = p[2]
 
-    def p_structured_dict(p):
+    def p_structured_dict(self, p):
         ''' structured_dict: structured_dict_item
                             | structured_dict structured_dict_item
                             | empty'''
@@ -125,7 +126,7 @@ class YamlParser(object):
            (key, val) = p[3]
            p[0].add(key, val)
 
-    def p_structured_dict_item(p):
+    def p_structured_dict_item(self, p):
         ''' structrured_dict_item: string COLOM inline_literal'''
         p[0] = (p[1], p[3])
 
